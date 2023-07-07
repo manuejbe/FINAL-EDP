2e1cd46943059791b54e8462ff010f3337dc6213
Manuel Ejberowicz <manuejbe@gmail.com>
2023-07-07 09:15:03 -0300

    agregue validacion en las opciones del main

--- main.py
+++ main.py
@@ -10,18 +10,18 @@ def menu():
     print("3. Desactivar router")
     print("4. Mostrar ruta")
     print("5. Salir")
-    opcion = input("Ingrese una opcion: ")
-    while opcion not in ["1","2","3","4","5"]:
-        opcion = input("Ingrese una opcion valida: ")
-    if opcion == "1":
+    opcion = validarIngresoNumerico("Ingrese una opcion: ")
+    while opcion not in range(1,6):
+        opcion = validarIngresoNumerico("Ingrese una opcion valida: ")
+    if opcion == 1:
         agregarRouterARuta(ruta1)
-    elif opcion == "2":
+    elif opcion == 2:
         activarRouter()
-    elif opcion == "3":
+    elif opcion == 3:
         desactivarRouter()
-    elif opcion == "4":
+    elif opcion == 4:
         mostrarRuta(ruta1)
-    elif opcion == "5":
+    elif opcion == 5:
         quit()
     menu()
 
