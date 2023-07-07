fe6bd51a8a7333809c65d6a9f9bfcc6411d0fa13
Manuel Ejberowicz <manuejbe@gmail.com>
2023-07-07 10:39:17 -0300

    ,.,

--- clases/Ruta.py
+++ clases/Ruta.py
@@ -1,3 +1,5 @@
+import random
+
 from clases.Router import Router
 
 class Ruta:
@@ -7,3 +9,16 @@ class Ruta:
 
     def agregarRouter(self,Router: Router):
         self.routers.append(Router)
+
+    def inicializarRutaRandom (self):
+        cantidadRouters = random.randint(4,10)
+        for i in range(cantidadRouters):
+            nombreRouter = "router_" + str(i)
+            router = Router(nombreRouter)
+            self.agregarRouter(router)
+        for router in self.routers:
+            activar = random.randint(0,1)
+            if activar == 1:
+                router.activar()
+            else:
+                router.desactivar()
